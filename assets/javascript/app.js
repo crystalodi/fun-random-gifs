3d38f950e9e13734e06493dbd2766f1ee3e3d994
Crystal O <crystalodi@gmail.com>
2018-03-10 21:31:57 -0600

    add submit button style and gif container rating style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,67 @@
-var buttonArray = ["the most popular girls in school", "rockos modern life", "gossip girl", "sailor moon", "hey arnold"];
+var topics = ["the most popular girls in school", "rockos modern life", "gossip girl", "sailor moon", "hey arnold"];
 
 function renderButtons() {
     $("#button-container").empty();
-    for(var i = 0; i < buttonArray.length; i++) {
+    for(var i = 0; i < topics.length; i++) {
         var button = $("<button class='gif-button'>");
-        button.text(buttonArray[i]);
-        button.attr("data-search-term", buttonArray[i]);
+        button.text(topics[i]);
+        button.attr("data-search-term", topics[i]);
+        button.attr("data-offset", 0);
         $("#button-container").append(button);
     }
 }
-
+$(".submit-button").on("click", function(event){
+    event.preventDefault();
+    var searchTerm = $("#searchTerm").val().trim();
+    if(searchTerm) {
+        topics.push(searchTerm);
+        renderButtons();
+    }
+});
 $(document.body).on("click", ".gif-button", function(){
     var tag = $(this).attr("data-search-term");
     var api_key = "AQ2G208I6ueekgMInH28ImZXlgHXik7a";
-    var queryURL = "https://api.giphy.com/v1/gifs/random" +
-    "?api_key=" + api_key + "&tag=" + tag;
+    var rating = "G"
+    var limit = 10;
+    var offset = parseInt($(this).attr("data-offset"));
+    var newOffset = offset + 10;
+    $(this).attr("data-offset", newOffset);
+    var queryURL = "https://api.giphy.com/v1/gifs/search" +
+    "?api_key=" + api_key + "&q=" + tag + "&rating=" + rating + "&limit=" + limit + "&offset=" + offset;
     $.ajax({
         url:queryURL,
         method: "GET"
     }).then(function(response){
-        //get the image url from the JSON object response
-        var imageUrl = response.data.image_original_url;
-
-        //create a new image on the page
-        var gifImage = $("<img>");
-
-        //set the src with the imageUrl variable
-        gifImage.attr("src", imageUrl);
-        gifImage.attr("alt", "gif");
-        gifImage.attr("class", "image-gif");
-        //add the most recent gif message to the top of the images container
-        $("#gif-container").prepend(gifImage);
+        var gifArray = response.data;
+        console.log(gifArray)
+        for(var i = 0; i < gifArray.length; i++) {
+            console.log(gifArray[i]);
+            var imageUrl = gifArray[i]["images"]["fixed_height_still"]["url"];
+            //create a new image on the page
+            var div = $("<div class='gif-img-container'>");
+            $("#gif-container").prepend(div);
+            var gifImage = $("<img>");
+            gifImage.attr("src", imageUrl);
+            gifImage.attr("alt", "gif");
+            gifImage.attr("class", "image-gif");
+            gifImage.attr("data-still", imageUrl);
+            gifImage.attr("data-animate", gifArray[i]["images"]["fixed_height"]["url"]);
+            gifImage.attr("data-state", "still");
+            var rating = $("<div class='rating'>");
+            rating.text("Rating: " + gifArray[i]["rating"].toUpperCase());
+            div.append(gifImage);
+            div.append(rating);
+        }
     });
-
 });
+$(document.body).on("click", ".image-gif", function(){
+    var state = $(this).attr("data-state");
+    if(state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate")
+    }else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still")
+    }
+})
 renderButtons();
